0c14364773dfe63ecbbdb2f9b4964b0c4f75fb4f
Paweł Hińcza <pawelsan@yahoo.com>
2022-01-07 18:49:59 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h1>Strona Główna</h1>;
+};
+
+export default HomePage;
---
+++ pages/lista-produktow.js
@@ -0,0 +1,5 @@
+const ProductList = () => {
+  return <h1>Lista produktów</h1>;
+};
+
+export default ProductList;
