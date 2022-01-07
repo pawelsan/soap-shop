e19673d00c63d3a9cc5d33412de1e03871e5f290
Paweł Hińcza <pawelsan@yahoo.com>
2022-01-07 21:39:39 +0100

    add Link component

--- pages/lista-produktow/index.js
+++ pages/lista-produktow/index.js
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 const ProductList = () => {
-  return <h1>Lista produktów</h1>;
+  return (
+    <>
+      <h1>Lista produktów</h1>
+      <ul>
+        <li>
+          <Link href="/lista-produktow/mydlo-a">Mydło A</Link>
+        </li>
+        <li>
+          <Link href="/lista-produktow/mydlo-b">Mydło B</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default ProductList;
