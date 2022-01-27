8bc37ec015bdffb6d2d7d42761615cf3978d95eb
Paweł Hińcza <pawelsan@yahoo.com>
2022-01-27 22:24:39 +0100

    add style-focused custom components and images to HomePage

---
+++ components/styled/index.js
@@ -0,0 +1,7 @@
+export const H2 = ({ children }) => {
+  return <h2 className="text-4xl border-b-4 pb-5 font-bold">{children}</h2>;
+};
+
+export const Paragraph = ({ children }) => {
+  return <p className="text-xl my-5">{children}</p>;
+};
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import { H2, Paragraph } from "../components/styled";
+
 const HomePage = () => {
   return (
     <>
-      <h2>Strona Główna</h2>
+      <H2>O Mydełkach Joasi</H2>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore illum
+        rem amet et quis quam, voluptatibus iusto accusamus error qui!
+      </Paragraph>
+      <div className="flex flex-col items-center">
+        <Image src="/images/home-2.jpg" width={1024} height={559} />
+      </div>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore illum
+        rem amet et quis quam, voluptatibus iusto accusamus error qui!
+      </Paragraph>
+      <div className="flex flex-col items-center">
+        <Image src="/images/home.jpg" width={1024} height={614} />
+      </div>
     </>
   );
 };
